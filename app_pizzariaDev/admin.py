f37e868ab7609c7231560e58942c3e97391d80a8
SnugbX <santoseverlan28@gmail.com>
2022-11-15 02:29:10 -0300

    ADD models CPagamento, Pedido, Entrega, Venda

--- app_pizzariaDev/admin.py
+++ app_pizzariaDev/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Profile)
+admin.site.register(Carrinho)
+admin.site.register(Categoria)
+admin.site.register(Endereco)
+admin.site.register(Entrega)
+admin.site.register(ItemDeProduto)
+admin.site.register(Pagamento)
+admin.site.register(Pedido)
+admin.site.register(Produto)
+admin.site.register(Venda)
+
+
+
 
 # Register your models here.
